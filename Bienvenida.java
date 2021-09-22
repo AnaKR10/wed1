70e67808004407f25404666c53576641196d6527
AnaKR10 <90656275+AnaKR10@users.noreply.github.com>
2021-09-21 19:25:17 -0500

    programa en java clase por

---
+++ Bienvenida.java
@@ -0,0 +1,12 @@
+/*
+Autor: JCRM
+Proposito: Clase inicial de ejemplo
+Fecha: 4/ Feb/2020
+*/
+
+public class Bienvenida {
+    
+    public static void main(String[] args) {
+        System.out.print("Bienvenido al mundo !!!");
+    }  
+}
